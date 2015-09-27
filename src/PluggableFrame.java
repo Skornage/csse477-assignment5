1dd4b5e15d4f8ad13827ca547b7686c90f8ba920
Andrew Davidson <davidsac@rose-hulman.edu>
2015-09-27 17:03:00 -0400

    fixed up our main console window

--- src/PluggableFrame.java
+++ src/PluggableFrame.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
@@ -10,11 +12,35 @@ public class PluggableFrame extends javax.swing.JFrame {
 	private static final long serialVersionUID = 8245314602864068840L;
 
 	public PluggableFrame() {
-		Dimension size = new Dimension(800, 600);
-		this.setPreferredSize(size);
-		JPanel p = new JPanel();
-		p.setPreferredSize(size);
+		
+		Dimension size = new Dimension(1400, 1000);
+		Dimension buttonPanelSize = new Dimension(250, 1);
+		Dimension pluggablePanelSize = new Dimension(600, 1);
+		
+		this.setSize(size);
+		this.setResizable(false);
+		
+		JPanel p1 = new JPanel();
+		p1.setLayout(new BorderLayout());
+		p1.setSize(size);
+		p1.setBackground(Color.ORANGE);
 
-		p.add(new BubbleComponent());
+		JPanel p2 = new JPanel();
+		p2.setPreferredSize(buttonPanelSize);
+		p2.setBackground(Color.black);
+
+		JPanel p3 = new JPanel();
+		p3.setPreferredSize(pluggablePanelSize);
+		p3.setBackground(Color.GREEN);
+		
+		
+
+		p3.add(new BubbleComponent());
+		
+		p1.add(p2, BorderLayout.WEST);
+		p1.add(p3,BorderLayout.CENTER);
+		
+		this.add(p1);
+		
 	}
 }
