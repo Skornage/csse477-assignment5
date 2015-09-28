b6dfe7daadab4c50550666d929565ff86ddf1333
Andrew Davidson <davidsac@rose-hulman.edu>
2015-09-28 19:37:23 -0400

    implemented more logic for loading classes

--- src/PluggableFrame.java
+++ src/PluggableFrame.java
@@ -1,8 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.io.File;
 import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
@@ -16,19 +15,18 @@ import javax.swing.event.ListSelectionListener;
 
 public class PluggableFrame extends javax.swing.JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8245314602864068840L;
-	private ArrayList<IPanelPlugin> plugins;
+	private ArrayList<IPanelPlugin> plugins = new ArrayList<IPanelPlugin>();
+	DefaultListModel<String> listModel = new DefaultListModel<String>();
 	private JComponent centralPanel;
+	private int currentPluginIndex = -1;
 
 	public PluggableFrame() {
 
+		loadPlugins();
+
 		Dimension size = new Dimension(1400, 1000);
 		Dimension buttonPanelSize = new Dimension(250, 1);
-
-		this.plugins = new ArrayList<IPanelPlugin>();
 
 		this.setSize(size);
 		this.setResizable(false);
@@ -42,17 +40,7 @@ public class PluggableFrame extends javax.swing.JFrame {
 		p2.setPreferredSize(buttonPanelSize);
 		p2.setBackground(Color.black);
 
-		// loadPlugins();
-
-		this.plugins.add(new JurassicTSwiftPanelPlugin());
-		this.plugins.add(new BubblePanelPlugin());
-
-		DefaultListModel<String> lm = new DefaultListModel<String>();
-		for (IPanelPlugin plugin : this.plugins) {
-			lm.addElement(plugin.getName());
-		}
-
-		JList<String> namesList = new JList<String>(lm);
+		JList<String> namesList = new JList<String>(listModel);
 		namesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		namesList.setLayoutOrientation(JList.VERTICAL);
 		namesList.setVisibleRowCount(-1);
@@ -93,10 +81,42 @@ public class PluggableFrame extends javax.swing.JFrame {
 		this.add(p1);
 	}
 
+	private void loadPlugins() {
+
+		File f = new File("/plugins");
+		File[] jarsToAdd = f.listFiles();
+
+		for (File jar : jarsToAdd) {
+			if (jar.isFile()) {
+				String name = jar.getName();
+				int i = name.lastIndexOf('.');
+				if (i > 0) {
+					String extension = name.substring(i + 1);
+					if (extension.toLowerCase().equals("jar")) {
+						// addPlugin(new JurassicTSwiftPanelPlugin());
+					}
+				}
+
+			}
+		}
+
+		addPlugin(new JurassicTSwiftPanelPlugin());
+		addPlugin(new BubblePanelPlugin());
+	}
+
+	private void addPlugin(IPanelPlugin plugin) {
+		this.plugins.add(plugin);
+		plugin.onCreate();
+		listModel.addElement(plugin.getName());
+	}
+
 	protected void selectPlugin(int index) {
 		this.centralPanel.removeAll();
 		this.centralPanel.add(this.plugins.get(index).getComponent());
+		this.plugins.get(index).onStart();
+		this.plugins.get(this.currentPluginIndex).onPause();
 		this.centralPanel.revalidate();
 		this.centralPanel.repaint();
+		this.currentPluginIndex = index;
 	}
 }
