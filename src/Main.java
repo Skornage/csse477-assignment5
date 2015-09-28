6ccad50eb782ded63f5cfbcd7fdc948cb9064fa1
Skornage <millerna@rose-hulman.edu>
2015-09-28 17:58:29 -0400

    Added plugin jar and file listener

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,53 @@
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.Watchable;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		PluggableFrame f = new PluggableFrame();
 		f.add(new BubbleComponent());
 		f.setVisible(true);
-		System.out.println("Testing");
+
+		Path myDir = Paths.get("plugins");
+
+		try {
+			WatchService watcher = myDir.getFileSystem().newWatchService();
+			myDir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+			WatchKey watckKey = watcher.take();
+
+			while (true) {
+				List<WatchEvent<?>> events = watckKey.pollEvents();
+				for (WatchEvent event : events) {
+					if (event.kind() == ENTRY_CREATE) {
+						System.out.println("Created: " + event.context().toString());
+					}
+					if (event.kind() == ENTRY_DELETE) {
+						System.out.println("Delete: "
+								+ event.context().toString());
+					}
+					if (event.kind() == ENTRY_MODIFY) {
+						System.out.println("Modify: "
+								+ event.context().toString());
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println("Error: " + e.toString());
+		}
 	}
 
 }
