d6e62bc54bdb0f9633ffc8029f0ac438636379d3
Andrew Davidson <davidsac@rose-hulman.edu>
2015-09-28 21:08:49 -0400

    combined clasloader into existing design

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,35 @@
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.List;
-import java.net.URL;
-
-import classLoader.JarClassLoader;
 
 public class Main {
 
 	public static void main(String[] args) {
 		PluggableFrame f = new PluggableFrame();
-		f.add(new BubbleComponent());
 		f.setVisible(true);
-		
-		try {
-			URL fileUrl = new File("plugins/test.jar").toURI().toURL();
-			
-			JarClassLoader jarLoader = new JarClassLoader(fileUrl);
-	        Class<?> c = jarLoader.loadClass("JurassicTSwiftPanelPlugin");
-	        Object o = c.newInstance();
-	        System.out.println("NAME: " + ((IPanelPlugin) o).getName());
-		}
-		catch (Exception e) {
-			System.out.println("Error: " + e.toString());
-		}
 
 		Path myDir = Paths.get("plugins");
 
 		try {
 			WatchService watcher = myDir.getFileSystem().newWatchService();
-			myDir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+			myDir.register(watcher, ENTRY_CREATE, ENTRY_DELETE);
 
 			WatchKey watckKey = watcher.take();
 			while (true) {
 				List<WatchEvent<?>> events = watckKey.pollEvents();
 				for (WatchEvent event : events) {
 					if (event.kind() == ENTRY_CREATE) {
-						System.out.println("Created: " + event.context().toString());
-					}
-					if (event.kind() == ENTRY_DELETE) {
-						System.out.println("Delete: "
-								+ event.context().toString());
-					}
-					if (event.kind() == ENTRY_MODIFY) {
-						System.out.println("Modify: "
-								+ event.context().toString());
+						f.loadPlugin(new File("plugins/"
+								+ event.context().toString()));
+					} else if (event.kind() == ENTRY_DELETE) {
+						f.removePlugin(event.context().toString());
 					}
 				}
 			}
--- src/PluggableFrame.java
+++ src/PluggableFrame.java
@@ -1,8 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -15,6 +13,8 @@ import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+
+import classLoader.JarClassLoader;
 
 public class PluggableFrame extends javax.swing.JFrame {
 
@@ -40,7 +40,7 @@ public class PluggableFrame extends javax.swing.JFrame {
 		JPanel p2 = new JPanel();
 		p2.setPreferredSize(buttonPanelSize);
 		p2.setBackground(Color.black);
-		
+
 		JList<String> namesList = new JList<String>(listModel);
 		namesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		namesList.setLayoutOrientation(JList.VERTICAL);
@@ -84,29 +84,6 @@ public class PluggableFrame extends javax.swing.JFrame {
 		loadPlugins();
 	}
 
-	private void loadPlugins() {
-
-		File f = new File("/plugins");
-		File[] jarsToAdd = f.listFiles();
-
-		for (File jar : jarsToAdd) {
-			if (jar.isFile()) {
-				String name = jar.getName();
-				int i = name.lastIndexOf('.');
-				if (i > 0) {
-					String extension = name.substring(i + 1);
-					if (extension.toLowerCase().equals("jar")) {
-						// addPlugin(new JurassicTSwiftPanelPlugin());
-					}
-				}
-
-			}
-		}
-
-		addPlugin(new JurassicTSwiftPanelPlugin());
-		addPlugin(new BubblePanelPlugin());
-	}
-
 	private void addPlugin(IPanelPlugin plugin) {
 		this.plugins.add(plugin);
 		plugin.onCreate();
@@ -117,9 +94,78 @@ public class PluggableFrame extends javax.swing.JFrame {
 		this.centralPanel.removeAll();
 		this.centralPanel.add(this.plugins.get(index).getComponent());
 		this.plugins.get(index).onStart();
-		this.plugins.get(this.currentPluginIndex).onPause();
+		if (this.currentPluginIndex >= 0) {
+			this.plugins.get(this.currentPluginIndex).onPause();
+		}
 		this.centralPanel.revalidate();
 		this.centralPanel.repaint();
 		this.currentPluginIndex = index;
 	}
+
+	protected void removePlugin(String name) {
+		System.out.println(name);
+		int i = name.lastIndexOf('.');
+		if (i > 0) {
+			String extension = name.substring(i + 1);
+			if (extension.toLowerCase().equals("jar")) {
+				String fileName = name.substring(0, i);
+				for (int j = 0; j < this.plugins.size(); j++) {
+					String className = this.plugins.get(j).getClass()
+							.getSimpleName();
+					if (fileName.equals(className)) {
+						if (this.currentPluginIndex == j) {
+							this.plugins.get(j).onDestroy();
+							this.currentPluginIndex = -1;
+							this.centralPanel.removeAll();
+							this.centralPanel.revalidate();
+							this.centralPanel.repaint();
+						}
+						this.plugins.remove(j);
+						this.listModel.remove(j);
+
+					}
+				}
+			}
+		}
+	}
+
+	// //////////////////////refactor out below///////////////////////
+
+	private void loadPlugins() {
+
+		File f = new File("plugins");
+		File[] jarsToAdd = f.listFiles();
+
+		if (jarsToAdd != null) {
+			for (File jar : jarsToAdd) {
+				loadPlugin(jar);
+			}
+		}
+
+	}
+
+	protected void loadPlugin(File jar) {
+		if (jar.isFile()) {
+			String name = jar.getName();
+			int i = name.lastIndexOf('.');
+			if (i > 0) {
+				String extension = name.substring(i + 1);
+				if (extension.toLowerCase().equals("jar")) {
+
+					try {
+						URL fileUrl = jar.toURI().toURL();
+
+						JarClassLoader jarLoader = new JarClassLoader(fileUrl);
+						Class<?> c = jarLoader.loadClass(name.substring(0, i));
+						Object o = c.newInstance();
+						addPlugin((IPanelPlugin) o);
+					} catch (Exception e) {
+						System.out.println("Error: " + e.toString());
+					}
+				}
+			}
+
+		}
+	}
+
 }
